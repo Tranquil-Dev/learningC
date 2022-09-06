d756ee83530ac6bd50d213340790a531bc3a7e1a
Abdulqudus-Adamson <abdulqudusadamson@gmail.com>
2022-09-06 03:54:13 +0100

    added file.c

---
+++ file.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main()
+{
+    // // C variables
+        // char name = 'A';
+    
+    // // Format identifiers, it starts with %
+        // printf("%c", name);
+    
+    // // Using the format identifiers
+        // int favNum = 10;
+        // char favLet = 'A';
+
+        // printf("My favorite number is %i and my favorite letter is %c",favNum,favLet);
+
+    // // Adding variables together
+        // int firstNum = 10;
+        // int secondNum = 15;
+        // int sum = firstNum + secondNum;
+        // printf("sum = %d", sum);
+
+    // // Declaring multiple variables
+        // int a = 9 , b = 44 , c = 10;
+        // printf("%d", a+b+c);
+
+    // // 
+    double num = 0.8888888;
+    printf("%f", num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
